3b7079d0298df27df8a530c8ce1d38e8f599ad63
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-10-16 14:13:55 +0900

    01_02 確認課題

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include<stdio.h>
 
-int main() {
+int Recursive(int n) {
+	return n * 1072;
+}
 
-	printf_s("あいうえお");
+int Recursive2(int n) {
+	if (n <= 1) {
+		return (100);
+	}
+	return (Recursive(n - 1) * 2 - 50);
+}
+
+int main() {
+	int n = 3;
+	int result{};
+	int result2{};
+
+	result = Recursive(n);
+	result2 = Recursive2(n);
+
+	printf("一般的な資金体系で%d時間 = %d\n", n, result);
+	printf("再帰的な資金体系で%d時間 = %d\n", n, result2);
 
 	return 0;
 }
