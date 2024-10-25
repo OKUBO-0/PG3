d2fe63ced03320f9167fd8cc6b4bfa43c1927dc8
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-10-25 12:07:37 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <time.h>    
 #include <windows.h> 
 
-int rollDice() {
+int diceRoll() {
 	return rand() % 6 + 1;
 }
 
-void showResult(int* result) {
+void Result(int* result) {
 	if (*result == 1) {
 		printf("正解\n");
 	}
@@ -16,7 +16,7 @@ void showResult(int* result) {
 	}
 }
 
-void setTimeout(void (*pFunc)(int*), int seconds, int* result) {
+void setTime(void (*pFunc)(int*), int seconds, int* result) {
 	Sleep(seconds * 1000);
 	pFunc(result);
 }
@@ -32,7 +32,7 @@ int main() {
 	int num, dice, ans;
 	captureInput(num);
 
-	dice = rollDice();
+	dice = diceRoll();
 	printf("判定中...\n");
 
 	if ((dice % 2 == 0 && num == 2) || (dice % 2 != 0 && num == 1)) {
@@ -42,7 +42,7 @@ int main() {
 		ans = 0;
 	}
 
-	setTimeout(showResult, 3, &ans);
+	setTime(Result, 3, &ans);
 	printf("サイコロの出目は%d でした\n", dice);
 
 	return 0;
