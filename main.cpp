f5a765674f2bf9b55dc28293c93feaa785386dcf
OKUBO-0 <k023g0020@g.neec.ac.jp>
2024-11-01 12:51:54 +0900

    02_03 確認課題

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
     Animal* myDog = new Dog(); // Dogのインスタンスを生成
     Animal* myCat = new Cat(); // Catのインスタンスを生成
 
-    myDog->makeSound(); // "ワン! ワン!" 　　と出力
-    myCat->makeSound(); // "ニャン! ニャン!" と出力
+    myDog->makeSound(); // "ワン! ワン!" 　　 と出力
+    myCat->makeSound(); // "ニャン! ニャン!"  と出力
 
     // メモリ解放
     delete myDog;
